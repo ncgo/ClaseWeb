8225b0990bcf9e14942e11ed771548b54acd9c76
nadia.co <ngarciaorozco@outlook.com>
2021-09-30 13:05:42 -0600

    Lab7 answers worked in class

--- Lab7/script.js
+++ Lab7/script.js
@@ -1,7 +1,51 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  var animals = ['dog', 'cat', 'rabbit', 'frog', 'chicken', 'bird', 'turtle']
 
-// Start your code from here
+  populateButtons(animals, 'animal-button', '#animal-buttons')
 
+  function populateButtons(arrayToUse, classToAdd, placeholder) {
+    $(placeholder).empty()
+    for (var i = 0; i < animals.length; i++) {
+      var a = $('<button>')
+      a.addClass(classToAdd)
+      a.attr('data-type', arrayToUse[i])
+      a.text(arrayToUse[i])
+      $(placeholder).append(a)
+    }
+  }
 
+  $('#animal-buttons').on('click', '.animal-button', function () {
+    $('#animals').empty()
+    var search = $(this).attr('data-type')
+    var queryUrl =
+      'https://api.giphy.com/v1/gifs/search?q=' +
+      search +
+      '&api_key=lv4LRtLumJVOd0Vai1fTOLtVXmRMDUns'
+    $.ajax({ url: queryUrl }).then(function (response) {
+      console.log(response)
+      var results = response.data
+      for (var i = 0; i < results.length; i++) {
+        var animalDiv = $('<div class="animal-item">')
+        var rating = results[i].rating
+        var p = $('<p>').text('Rating: ' + rating)
 
-});
+        var animated = results[i].images.fixed_height.url
+        var still = results[i].images.fixed_height_still.url
+
+        var animalImage = $('<img>')
+        animalImage.attr('src', still)
+        animalImage.attr('data-still', still)
+        animalImage.attr('data-animate', animated)
+        animalImage.attr('data-isAnimated', 'false')
+        animalImage.addClass('animal-image')
+
+        animalDiv.append(p)
+        animalDiv.append(animalImage)
+
+        $('#animals').append(animalDiv)
+      }
+    })
+  })
+
+  //if (state )
+})
